e1442afd2dc4056f3383de6bd6c40820c82ca594
wxutopia <wangx2167@163.com>
2025-07-06 09:29:58 +0800

    Neovim: change loading event for some plugins.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  event = { "BufReadPost", "BufNewFile" },
   build = ':TSUpdate',
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
@@ -25,8 +26,8 @@ return {
     highlight = {
       enable = true,
       -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+      -- If you are experiencing weird indenting issues, add the language to
+      -- the list of additional_vim_regex_highlighting and disabled languages for indent.
       additional_vim_regex_highlighting = { 'ruby' },
     },
     indent = { enable = false, disable = { 'ruby' } },
