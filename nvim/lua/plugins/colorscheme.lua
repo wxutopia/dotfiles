ce5740a42a1d979c5fef0f4f9412dedb39f098a7
wxutopia <wangx2167@163.com>
2025-07-14 18:59:49 +0800

    Neovim: Change indent to 4.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -17,10 +17,10 @@
 -- }
 
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme catppuccin-frappe")
-  end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		vim.cmd("colorscheme catppuccin-frappe")
+	end,
 }
