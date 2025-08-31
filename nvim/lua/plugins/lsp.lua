6c50d384a87a90d6b2fc4709b766d7fbcaea83e6
wxutopia <wangx2167@163.com>
2025-08-31 15:40:48 +0800

    Neovim: Add CMake language server.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -184,5 +184,21 @@ return {
 				},
 			},
 		})
+
+		vim.lsp.config("cmake", {
+			cmd = { "cmake-language-server" },
+			filetypes = { "cmake" },
+			init_options = { buildDirectory = "build" },
+			root_markers = {
+				"CMakePresets.json",
+				"CTestConfig.cmake",
+				".git",
+				"build",
+				"cmake",
+				".clangd",
+				".clang-tidy",
+				".clang-format",
+			},
+		})
 	end,
 }
