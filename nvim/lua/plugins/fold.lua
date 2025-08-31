28fdaca274b6133da25e8c7161ed43134ffdb9b7
wxutopia <wangx2167@163.com>
2025-08-31 16:32:29 +0800

    Neovim: Add treesitter as the provider of nvim-ufo.

--- nvim/lua/plugins/fold.lua
+++ nvim/lua/plugins/fold.lua
@@ -3,5 +3,11 @@ return {
 	dependencies = {
 		"kevinhwang91/promise-async",
 	},
-	opts = {},
+	config = function()
+		require("ufo").setup({
+			provider_selector = function(bufnr, filetype, buftype)
+				return { "treesitter", "indent" }
+			end,
+		})
+	end,
 }
